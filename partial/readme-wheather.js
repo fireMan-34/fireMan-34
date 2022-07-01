00591634b19c6fca59658eaad3805563a0590f89
fireMan-34 <524915379@qq.com>
2022-07-02 00:09:04 +0800

    fix:maybe fix cache pnpm error3

--- partial/readme-wheather.js
+++ partial/readme-wheather.js
@@ -1,7 +1,7 @@
 async function getWheather() {
     return new Promise((resolve) => setTimeout(() => resolve(), 2000)).then(
         data => {
-            return 'workflow will change it ,fileMan-34';
+            return 'workflow will change it ,fileMan-34' + Date.now().toLocaleString();
         }
     )
 }
