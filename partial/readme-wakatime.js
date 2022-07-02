00d77abf8602848c4818cce9c5dc11aa034640b6
fireMan-34 <524915379@qq.com>
2022-07-02 13:25:43 +0800

    docs:insert html code

---
+++ partial/readme-wakatime.js
@@ -0,0 +1,9 @@
+const wakatimeImages = [
+    '# Weaktime',
+    '- [![wakatime](https://wakatime.com/badge/user/a34ca63b-ead6-4b13-8b8f-ca28ee4db1bb/project/4791a7b9-444a-4381-807b-bbaea3a58cad.svg)](https://wakatime.com/badge/user/a34ca63b-ead6-4b13-8b8f-ca28ee4db1bb/project/4791a7b9-444a-4381-807b-bbaea3a58cad) `fireMan-34`',
+    '- [![wakatime](https://wakatime.com/badge/user/a34ca63b-ead6-4b13-8b8f-ca28ee4db1bb/project/d20969b8-965a-48f6-b080-e49a2ac5e5b8.svg)](https://wakatime.com/badge/user/a34ca63b-ead6-4b13-8b8f-ca28ee4db1bb/project/d20969b8-965a-48f6-b080-e49a2ac5e5b8) `project`',
+    '',
+    '# WorkTime',
+    '<figure><embed src="https://wakatime.com/share/@a34ca63b-ead6-4b13-8b8f-ca28ee4db1bb/3b1bfc70-e366-407b-948a-67a266f07aef.svg"></embed></figure>'
+]
+module.exports = wakatimeImages.join('\n\n');
